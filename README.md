e643acbc78c25de421e168ad7e762e86a50b56b3
Thomas Lee <tleehoc@gmail.com>
2014-12-26 20:19:28 -0800

    Add Pushbullet notification to checker.

---
+++ README.md
@@ -0,0 +1,3 @@
+has-nexus6-on-moto
+===
+Pushbullet API token should placed in a file named .secret-api-token.
--- check.sh
+++ check.sh
@@ -9,6 +9,7 @@ function run() {
 			echo "64GB CW AVAILABLE!"
 			osascript -e 'display notification "MAKE IT HAPPEN - AVAILABLE" with title "NEXUS"'
 			open "https://www.motorola.com/on/demandware.store/Sites-Motorola_US-Site/en_US/CART_SVC_AddToCart-Service?pid=00637NARTL&showCart=1&qty=1"
+			curl -u `cat .secret-api-token`: -X POST https://api.pushbullet.com/v2/pushes --header 'Content-Type: application/json' --data-binary '{"type": "link", "title": "Nexus 6 64CW Available!", "body": "Add to cart", "url": "www.motorola.com/on/demandware.store/Sites-Motorola_US-Site/en_US/CART_SVC_AddToCart-Service?pid=00637NARTL&showCart=1&qty=1"}'
 			break;
 		fi
 		sleep 2
