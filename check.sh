cd07713744564f9c5a349dc6a3f59239e3191989
Thomas Lee <tleehoc@gmail.com>
2014-12-26 21:52:02 -0800

    Simplify Pushbullet API token access and add progress indicator.

--- check.sh
+++ check.sh
@@ -2,14 +2,16 @@
 date
 function run() {
 	IS_AVAILABLE=0
+	API_TOKEN=`cat .secret-api-token`
 	while [ $IS_AVAILABLE -eq 0 ]
 	do
 		IS_AVAILABLE=`curl http://www.motorola.com/us/consumers/nexus-6-header/Nexus-6/nexus-6-motorola-us.html --no-keepalive -s | grep -i -m1 "00637NARTL\":{[^}]\\+availability\":\"IN_STOCK" | wc -l`
+		printf "."
 		if [ $IS_AVAILABLE -gt 0 ]; then
 			echo "64GB CW AVAILABLE!"
 			osascript -e 'display notification "MAKE IT HAPPEN - AVAILABLE" with title "NEXUS"'
 			open "https://www.motorola.com/on/demandware.store/Sites-Motorola_US-Site/en_US/CART_SVC_AddToCart-Service?pid=00637NARTL&showCart=1&qty=1"
-			curl -u `cat .secret-api-token`: -X POST https://api.pushbullet.com/v2/pushes --header 'Content-Type: application/json' --data-binary '{"type": "link", "title": "Nexus 6 64CW Available!", "body": "Add to cart", "url": "www.motorola.com/on/demandware.store/Sites-Motorola_US-Site/en_US/CART_SVC_AddToCart-Service?pid=00637NARTL&showCart=1&qty=1"}'
+			curl -u "$API_TOKEN": -X POST https://api.pushbullet.com/v2/pushes --header 'Content-Type: application/json' --data-binary '{"type": "link", "title": "Nexus 6 64CW Available!", "body": "Add to cart", "url": "www.motorola.com/on/demandware.store/Sites-Motorola_US-Site/en_US/CART_SVC_AddToCart-Service?pid=00637NARTL&showCart=1&qty=1"}'
 			break;
 		fi
 		sleep 2
